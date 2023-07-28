c2f19785099d028595cd597542bea32c64c46eb4
DESKTOP-4ST7896\toybo <toyboy0113@naver.com>
2023-07-28 15:21:09 +0900

    fix: 새로고침시 로그인 상태가 풀려 메인페이지로 이동하는 버그

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,62 @@ import { Outlet } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { REGENERATION_REFRESH_API_PATH } from './constants/api/userApi';
 import { accessTokenAtom } from './recoil/userAtoms';
-import useHttpRequest from './hooks/useHttpRequest';
 import useLogout from './hooks/useLogout';
 import Loading from './components/shared/Loading';
+import axios, { AxiosResponse } from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 interface ResultData {
   accessToken: string;
+}
+
+interface IresponseData {
+  isSuccess: boolean;
+  code: number;
+  message: string;
+  result: ResultData;
 }
 
 function App() {
   const { logoutHandler } = useLogout();
   const setAccessToken = useSetRecoilState(accessTokenAtom);
   const [isCheckLoginState, setIsCheckLoginState] = useState(false);
-  const { responseData, sendRequest, isLoading } = useHttpRequest<ResultData>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [, setErrorMessage] = useState<string>('');
 
   const sendRefreshToken = async () => {
-    await sendRequest({ url: `${REGENERATION_REFRESH_API_PATH}`, withcredential: true });
+    setIsLoading(true);
+    setIsCheckLoginState(false);
+    setErrorMessage('');
+    try {
+      const response: AxiosResponse<IresponseData> = await axios({
+        url: `${API_URL}${REGENERATION_REFRESH_API_PATH}`,
+        withCredentials: true,
+        method: 'GET',
+      });
+      if (response.data.isSuccess) {
+        setAccessToken(response.data.result.accessToken);
+      } else {
+        logoutHandler();
+      }
+    } catch (error: any) {
+      if (error.response) {
+        setErrorMessage(error.response.data.message);
+      } else if (error.request) {
+        setErrorMessage(error.message);
+      }
+      setErrorMessage(error.response.data.message);
+    } finally {
+      setIsLoading(false);
+      setIsCheckLoginState(true);
+    }
   };
 
   useEffect(() => {
     sendRefreshToken();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  useEffect(() => {
-    setIsCheckLoginState(true);
-    if (!responseData) return;
-    if (responseData.isSuccess) {
-      setAccessToken(responseData.result.accessToken);
-    } else {
-      logoutHandler();
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [responseData]);
 
   if (isLoading || !isCheckLoginState) {
     return <Loading />;
